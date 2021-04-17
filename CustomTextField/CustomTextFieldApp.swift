9bb3e3d9b342a1f2b17787a6097ae1f748afa39c
Evgeny <buze@ya.ru>
2021-04-17 17:41:56 +0800

    Initial Commit

---
+++ CustomTextField/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  CustomTextField
+//
+//  Created by Evgeny on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
